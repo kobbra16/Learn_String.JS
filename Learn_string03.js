cba50849ac9f09f839e0690ba6584ebbe6d7ed00
unknown <dalas10@mail.ru>
2022-07-12 12:46:51 +0300

    learn_string.js

---
+++ Learn_string03.js
@@ -0,0 +1,45 @@
+// const name = 'Maxim'
+// const age = 34
+
+
+// function getAge() {
+//     return age
+// }
+
+// // //const output = ' Hi,my name is  ' + name + ' and my age ' + age + ' year .'
+// // const output = ` Hi,my name is ${name} and my age ${age<35 ? 'A':'B'} year `
+// // console.log(output)
+
+
+// const name = 'Maxim'
+// console.log(name.length) //покажет длинну слова Maxim
+// console.log(name.toLowerCase()) //выведет слово Maxim большимим буквами
+// console.log(name.toUpperCase()) //выведет слово Maxim маленькими буквами
+// console.log(name.charAt(2)) //значение 2 символа(отсчет с 0)
+// console.log(name.indexOf('xim')) //есть ли значение xim
+// console.log(name.startsWith('Max')) //начало на Max
+// console.log(name.endsWith('im')) //окончание на im
+// console.log(name.repeat(3)) //повторит в консоли 3 раза Maxim
+// const string = '    password     ' // password с пробелами
+// console.log(string)
+// console.log(string.trim()) //уберет пробелы
+// console.log(string.trimLeft()) //уберет пробелы слева
+// console.log(string.trimRight()) // уберет пробуелы справа
+
+
+
+// function logPerson(s, name, age) {
+//     if (age < 0) {
+//         age = 'Not yet born'
+//     }
+//     return `${s[0]}${name}${s[1]}${age}${s[2]}`
+// }
+// const personName = ' Maxim '
+// const personAge = 34
+// const personName2 = ' Volodia '
+// const personAge2 = -10
+
+// const output2 = logPerson `name: ${personName2},age:${personAge2}!`
+// const output = logPerson `name: ${personName},age:${personAge}!`
+// console.log(output)
+// console.log(output2)
